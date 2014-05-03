9c38fbf728fef8a7620b2e868846c910b10f6227
Morgan Phillips <morgan@artistgrowth.com>
2014-05-03 14:37:48 -0500

    Adds deploy scripts.

---
+++ deployment/pull-latest.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env sh
+
+####
+# Automates the process of pulling the lateset production branch.
+# Adding the argument server finishes by running a rails server in production mode (for testing)
+# Deploy will go ahead and deploy the latest branch to Heroku
+###
+
+if [ which rbenv ]; then
+    RUBY_DIR=~/.rbenv/versions/2.1.1/bin/
+    git pull
+    $RUBY_DIR/bundle install
+    $RUBY_DIR/rake db:migrate
+    if [ "$1" = "deploy" ];
+        then ./deploy.sh
+    fi
+    if [ "$1" = "server" ];
+        then $RUBY_DIR/rails server -e production;
+    fi
+else
+    echo "This script requires rbenv.";
+fi
