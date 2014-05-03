5bdbff219a778f9e824ca51cf7e35e88461760e5
Ashley Wright <AshleyMarieEvans@gmail.com>
2014-05-03 16:53:55 -0500

    Mentee Model with Devise

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140503161056) do
+ActiveRecord::Schema.define(version: 20140503205209) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,6 +25,24 @@ ActiveRecord::Schema.define(version: 20140503161056) do
     t.datetime "created_at"
     t.datetime "updated_at"
   end
+
+  create_table "mentees", force: true do |t|
+    t.string   "email",                  default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          default: 0,  null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "mentees", ["email"], name: "index_mentees_on_email", unique: true, using: :btree
+  add_index "mentees", ["reset_password_token"], name: "index_mentees_on_reset_password_token", unique: true, using: :btree
 
   create_table "requests", force: true do |t|
     t.datetime "created_at"
