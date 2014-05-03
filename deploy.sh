478b56f1eff324e6fa05f88aa9bdf9c264a4891f
Morgan Phillips <morgan@artistgrowth.com>
2014-05-03 17:08:34 -0500

    Removed redundant deploy.sh

--- deploy.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env sh
-
-if ! which heroku
-    then echo "To deploy you'll need to install and setup the heroku toolchain.";
-    else git pull; git push heroku master; heroku run rake db:migrate; heroku restart;
-fi
