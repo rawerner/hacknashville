e09b3e3a97ba5dffe207eed5fdafdae17cec8719
Rachel Werner <rachelannwerner@gmail.com>
2014-05-04 15:18:49 -0500

    mentor model working

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,6 @@ Rails.application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
   resources :mentors
-  devise_for :mentees
   resources :mentees
   # You can have the root of your site routed with "root"
   root 'welcome#index'
