b97202ffe2c2bac68cce1ed10f20d2ecfda64f33
Ashley Wright <AshleyMarieEvans@gmail.com>
2014-05-04 16:04:23 -0500

    Delete unnecessary code from Mentee controller

--- app/controllers/mentees_controller.rb
+++ app/controllers/mentees_controller.rb
@@ -1,28 +1,3 @@
 class MenteesController < ApplicationController
 
-  def create
-    mentee = Mentee.create(mentee_params)
-  end
-
-  def index
-    @mentees = Mentee.all
-  end
-
-  def new
-  end
-
-  def edit
-  end
-
-  def show
-    @mentee = Mentee.find(params[:id])
-  end
-
-  private
-
-  def mentee_params
-    params.require(:mentee).permit(:interest)
-  end
-
-
 end
